418cdb3acac68a758fa50621ac271eb8095d2789
Gabriela Reis <100102288+GabbiReis@users.noreply.github.com>
2023-09-26 20:16:48 -0300

    Add files via upload

---
+++ PapaiNoel/Controllers/CartinhaPapaiNoelController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PapaiNoel.Models;
+
+namespace PapaiNoel.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CartinhaPapaiNoelController : ControllerBase
+    {
+        private readonly string _CartinhaNoel;
+
+        public CartinhaPapaiNoelController()
+        {
+            _CartinhaNoel= Path.Combine(Directory.GetCurrentDirectory(), "Data", "Cartinha.json");
+        }
+
+        #region Operaçoes arquivo
+
+        private List<DadosCartinhas> LerCartaDoArquivo()
+        {
+            if (!System.IO.File.Exists(_CartinhaNoel))
+            {
+                return new List<DadosCartinhas>();
+            }
+
+            string json = System.IO.File.ReadAllText(_CartinhaNoel);
+            return JsonConvert.DeserializeObject<List<DadosCartinhas>>(json);
+        }
+
+        private void EscreverCartasNoArquivo(List<DadosCartinhas> cartas)
+        {
+            string json = JsonConvert.SerializeObject(cartas);
+            System.IO.File.WriteAllText(_CartinhaNoel, json);
+        }
+
+        #endregion
+
+        #region POST
+
+        [HttpPost]
+        public IActionResult Post(DadosCartinhas dadosCartinhas)
+        {
+
+            var CartasRealizadas = LerCartaDoArquivo();
+
+            CartasRealizadas.Add(dadosCartinhas);
+
+            EscreverCartasNoArquivo(CartasRealizadas);
+
+            return Ok("Carta registrada com sucesso");
+        }
+
+        #endregion
+
+        #region GET 
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(LerCartaDoArquivo());
+        }
+
+        #endregion
+    }
+}
+   
---
+++ PapaiNoel/Models/Request/DadosCartinhas.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace PapaiNoel.Models
+{
+    public class DadosCartinhas
+    {
+        [Required(ErrorMessage = "O Nome é obrigatória.")]
+        [StringLength(255, MinimumLength = 3, ErrorMessage = "O Nome deve ter no maximo 255 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Infome o nome da Rua/Avenida.")]
+        public string Rua { get; set; }
+
+        [Required(ErrorMessage = "Informe o numero de sua residencia.")]
+        public int Numero { get; set; }
+
+        [Required(ErrorMessage = "Informe seu bairro.")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "Informe sua cidade.")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Campo idade é obrigatório.")]
+        [Range(0, 15, ErrorMessage = "Somente maiores de 15 anos")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "O campo em questão é obrigatório.")]
+        [MaxLength(500, ErrorMessage = "O Número máximo de carateres atingido(500).")]
+        public string TextoCarta { get; set; }
+
+    }
+}
